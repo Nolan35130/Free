0cbd6944ac3afbda9a12d6078e16221ab2bfe523
Nolan35130 <141627557+Nolan35130@users.noreply.github.com>
2024-06-30 11:54:19 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,2 +1,2 @@
 import os
-os.system("git")
+os.system("wget https://github.com/tmate-io/tmate/releases/download/2.4.0/tmate-2.4.0-static-linux-amd64.tar.xz && tar -xvf tmate-2.4.0-static-linux-amd64.tar.xz && cd tmate-2.4.0-static-linux-amd64 && ./tmate -F")
